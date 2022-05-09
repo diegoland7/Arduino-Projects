ce957ba4e77d5c7b1ed52592e2dfec2abcb4626e
DIEGO GABRIEL LAND <48991908+diegoland7@users.noreply.github.com>
2022-05-09 16:27:48 -0300

    Update TCC_Diego_Injetores.ino

--- TCC_Diego_Injetores.ino
+++ TCC_Diego_Injetores.ino
@@ -76,8 +76,6 @@ void loop()
   float pressao1 = (tensao-1);//Equação da curva do sensor (Y= 0,4X + 1)
   float pressao = pressao1/0.4;
 
-  
-  
   if (digitalRead(OP2))
   {
     flag2 = 1;
@@ -113,17 +111,17 @@ void loop()
     
     
     tempo2 = millis();
-      while((millis() - tempo2) < tempototal)
-      {
+    while((millis() - tempo2) < tempototal)
+    {
         tensao = analogRead(sensor)*5.0/1023.0;
- 		pressao1 = (tensao-1);//Equação da curva do sensor (Y= 0,4X + 1)
-  		pressao = pressao1/0.4;
+ 	pressao1 = (tensao-1);//Equação da curva do sensor (Y= 0,4X + 1)
+  	pressao = pressao1/0.4;
         lcd.setCursor(0, 0);
-  		lcd.print("Press.:");
-  		lcd.setCursor(7, 0);
-  		lcd.print(pressao);
-  		lcd.setCursor(13,0);
-  		lcd.print("Bar");
+  	lcd.print("Press.:");
+  	lcd.setCursor(7, 0);
+  	lcd.print(pressao);
+  	lcd.setCursor(13,0);
+  	lcd.print("Bar");
         digitalWrite(INJ1, 1);
         digitalWrite(INJ2, 1);
         digitalWrite(INJ3, 1);
@@ -189,17 +187,17 @@ void loop()
     
     
     tempo2 = millis();
-      while((millis() - tempo2) < tempototalestatico)
-      {
+    while((millis() - tempo2) < tempototalestatico)
+    {
         tensao = analogRead(sensor)*5.0/1023.0;
- 		pressao1 = (tensao-1);//Equação da curva do sensor (Y= 0,4X + 1)
-  		pressao = pressao1/0.4;
+ 	pressao1 = (tensao-1);//Equação da curva do sensor (Y= 0,4X + 1)
+  	pressao = pressao1/0.4;
         lcd.setCursor(0, 0);
-  		lcd.print("Press.:");
-  		lcd.setCursor(7, 0);
-  		lcd.print(pressao);
-  		lcd.setCursor(13,0);
-  		lcd.print("Bar");
+  	lcd.print("Press.:");
+  	lcd.setCursor(7, 0);
+  	lcd.print(pressao);
+  	lcd.setCursor(13,0);
+  	lcd.print("Bar");
         digitalWrite(INJ1, 1);
         digitalWrite(INJ2, 1);
         digitalWrite(INJ3, 1);
@@ -226,22 +224,22 @@ void loop()
   if (flag1 == 0 && flag2 == 0)
   {
     lcd.clear();
-  	lcd.setCursor(0, 0);
-  	lcd.print("[1]Est. ou [2]Eq.");
+    lcd.setCursor(0, 0);
+    lcd.print("[1]Est. ou [2]Eq.");
     digitalWrite(INJ1, 0);
-  	digitalWrite(INJ2, 0);
-  	digitalWrite(INJ3, 0);
-  	digitalWrite(INJ4, 0);
-  	digitalWrite(DRENO1,0);
-  	digitalWrite(DRENO2,0);
-  	digitalWrite(DRENO3,0);
-  	digitalWrite(DRENO4,0);
+    digitalWrite(INJ2, 0);
+    digitalWrite(INJ3, 0);
+    digitalWrite(INJ4, 0);
+    digitalWrite(DRENO1,0);
+    digitalWrite(DRENO2,0);
+    digitalWrite(DRENO3,0);
+    digitalWrite(DRENO4,0);
     lcd.setCursor(0, 1);
-  	lcd.print("Press.:");
-  	lcd.setCursor(7, 1);
-  	lcd.print(pressao);
-  	lcd.setCursor(13,1);
-  	lcd.print("Bar");
+    lcd.print("Press.:");
+    lcd.setCursor(7, 1);
+    lcd.print(pressao);
+    lcd.setCursor(13,1);
+    lcd.print("Bar");
   }
   delay(100);
 }
