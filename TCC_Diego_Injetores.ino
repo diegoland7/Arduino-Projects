13bf621ab3f41c86f8cc3a7d2fa06dd2c38249d3
DIEGO GABRIEL LAND <48991908+diegoland7@users.noreply.github.com>
2022-05-10 10:27:09 -0300

    Update TCC_Diego_Injetores.ino

--- TCC_Diego_Injetores.ino
+++ TCC_Diego_Injetores.ino
@@ -1,27 +1,27 @@
 #include <LiquidCrystal.h>
 
-#define INJ1 13
-#define INJ2 12
-#define INJ3 11
-#define INJ4 10
+#define INJ1 41
+#define INJ2 43
+#define INJ3 45
+#define INJ4 47
 
-#define OP1 14
-#define OP2 15
+#define OP1 24
+#define OP2 26
 
-#define DRENO1 17
-#define DRENO2 18
-#define DRENO3 19
-#define DRENO4 7
+#define DRENO1 31
+#define DRENO2 33
+#define DRENO3 35
+#define DRENO4 37
 
 //////////////////////////Pinos Display//////////////////////////////
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 ////////////////////////////////////////////////////////////////////
 
-unsigned long tempototal = 10000;
-unsigned long tempototalestatico = 5000;
+unsigned long tempototal = 60000;
+unsigned long tempototalestatico = 20000;
 unsigned long tempo2 = 0;
-unsigned long tempoentrepulsos = 60;
-int sensor = A2;
+unsigned long tempoentrepulsos = 30;
+int sensor = A15;
 int flag2 = 0;
 int flag1 = 0;
 
@@ -45,10 +45,10 @@ void setup()
   digitalWrite(INJ2, 0);
   digitalWrite(INJ3, 0);
   digitalWrite(INJ4, 0);
-  digitalWrite(DRENO1,0);
-  digitalWrite(DRENO2,0);
-  digitalWrite(DRENO3,0);
-  digitalWrite(DRENO4,0);
+  digitalWrite(DRENO1,1);
+  digitalWrite(DRENO2,1);
+  digitalWrite(DRENO3,1);
+  digitalWrite(DRENO4,1);
   
   lcd.begin(16, 2);
   lcd.clear();
@@ -72,19 +72,19 @@ void loop()
   float pressao1 = (tensao-1);//Equação da curva do sensor (Y= 0,4X + 1)
   float pressao = pressao1/0.4;
 
-  if (digitalRead(OP2))
+  if (digitalRead(OP1) == 1)
   {
-    flag2 = 1;
+    flag1 = 1;
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("**Removendo Ar**");
     lcd.setCursor(0, 1);
     lcd.print("Iniciando......");
     digitalWrite(INJ1, 1);
-    digitalWrite(DRENO1,1);
-    digitalWrite(DRENO2,1);
-    digitalWrite(DRENO3,1);
-    digitalWrite(DRENO4,1);
+    digitalWrite(DRENO1,0);
+    digitalWrite(DRENO2,0);
+    digitalWrite(DRENO3,0);
+    digitalWrite(DRENO4,0);
     delay(1000);
     digitalWrite(INJ1, 0);
     digitalWrite(INJ2, 1);
@@ -96,12 +96,80 @@ void loop()
     digitalWrite(INJ4, 1);
     delay(1000);
     digitalWrite(INJ4, 0);
+    digitalWrite(DRENO1,1);
+    digitalWrite(DRENO2,1);
+    digitalWrite(DRENO3,1);
+    digitalWrite(DRENO4,1);
+    lcd.clear();
+  
+    lcd.setCursor(0, 1);
+    lcd.print("TEMPO(s): ");
+    
+
+    tempo2 = millis();
+    while((millis() - tempo2) < tempototalestatico)
+    {
+        digitalWrite(INJ1, 1);
+        digitalWrite(INJ2, 1);
+        digitalWrite(INJ3, 1);
+        digitalWrite(INJ4, 1);
+        tensao = analogRead(sensor)*5.0/1023.0;
+        pressao1 = (tensao-1);//Equação da curva do sensor (Y= 0,4X + 1)
+        pressao = pressao1/0.4;
+        lcd.setCursor(0, 0);
+        lcd.print("Press.:");
+        lcd.setCursor(7, 0);
+        lcd.print(pressao);
+        lcd.setCursor(13,0);
+        lcd.print("Bar");
+        //delay(tempototalestatico);
+        lcd.setCursor(10, 1);
+        int strr1 = (tempototalestatico-(millis() - tempo2))/1000;
+        String strr2 = " ";
+        String str1 =  strr1 + strr2;
+        lcd.print(str1);
+        if (digitalRead(OP1) && flag1 == 1)
+        {
+          break;
+        }
+      }
+        digitalWrite(INJ1, 0);
+        digitalWrite(INJ2, 0);
+        digitalWrite(INJ3, 0);
+        digitalWrite(INJ4, 0);
+    flag1=0;
+
+  }
+  if (digitalRead(OP2))
+  {
+    flag2 = 1;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("**Removendo Ar**");
+    lcd.setCursor(0, 1);
+    lcd.print("Iniciando......");
+    digitalWrite(INJ1, 1);
     digitalWrite(DRENO1,0);
     digitalWrite(DRENO2,0);
     digitalWrite(DRENO3,0);
     digitalWrite(DRENO4,0);
+    delay(1000);
+    digitalWrite(INJ1, 0);
+    digitalWrite(INJ2, 1);
+    delay(1000);
+    digitalWrite(INJ2, 0);
+    digitalWrite(INJ3, 1);
+    delay(1000);
+    digitalWrite(INJ3, 0);
+    digitalWrite(INJ4, 1);
+    delay(1000);
+    digitalWrite(INJ4, 0);
+    digitalWrite(DRENO1,1);
+    digitalWrite(DRENO2,1);
+    digitalWrite(DRENO3,1);
+    digitalWrite(DRENO4,1);
     lcd.clear();
-	
+  
     lcd.setCursor(0, 1);
     lcd.print("TEMPO(s): ");
     
@@ -110,14 +178,14 @@ void loop()
     while((millis() - tempo2) < tempototal)
     {
         tensao = analogRead(sensor)*5.0/1023.0;
- 	pressao1 = (tensao-1);//Equação da curva do sensor (Y= 0,4X + 1)
-  	pressao = pressao1/0.4;
+        pressao1 = (tensao-1);//Equação da curva do sensor (Y= 0,4X + 1)
+        pressao = pressao1/0.4;
         lcd.setCursor(0, 0);
-  	lcd.print("Press.:");
-  	lcd.setCursor(7, 0);
-  	lcd.print(pressao);
-  	lcd.setCursor(13,0);
-  	lcd.print("Bar");
+        lcd.print("Press.:");
+        lcd.setCursor(7, 0);
+        lcd.print(pressao);
+        lcd.setCursor(13,0);
+        lcd.print("Bar");
         digitalWrite(INJ1, 1);
         digitalWrite(INJ2, 1);
         digitalWrite(INJ3, 1);
@@ -144,77 +212,6 @@ void loop()
       digitalWrite(INJ3, 0);
       digitalWrite(INJ4, 0);
       flag2 = 0;
-
-
-  }
-  
-  if (digitalRead(OP1) == 1)
-  {
-    flag1 = 1;
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("**Removendo Ar**");
-    lcd.setCursor(0, 1);
-    lcd.print("Iniciando......");
-    digitalWrite(INJ1, 1);
-    digitalWrite(DRENO1,1);
-    digitalWrite(DRENO2,1);
-    digitalWrite(DRENO3,1);
-    digitalWrite(DRENO4,1);
-    delay(1000);
-    digitalWrite(INJ1, 0);
-    digitalWrite(INJ2, 1);
-    delay(1000);
-    digitalWrite(INJ2, 0);
-    digitalWrite(INJ3, 1);
-    delay(1000);
-    digitalWrite(INJ3, 0);
-    digitalWrite(INJ4, 1);
-    delay(1000);
-    digitalWrite(INJ4, 0);
-    digitalWrite(DRENO1,0);
-    digitalWrite(DRENO2,0);
-    digitalWrite(DRENO3,0);
-    digitalWrite(DRENO4,0);
-    lcd.clear();
-	
-    lcd.setCursor(0, 1);
-    lcd.print("TEMPO(s): ");
-    
-    
-    tempo2 = millis();
-    while((millis() - tempo2) < tempototalestatico)
-    {
-        tensao = analogRead(sensor)*5.0/1023.0;
- 	pressao1 = (tensao-1);//Equação da curva do sensor (Y= 0,4X + 1)
-  	pressao = pressao1/0.4;
-        lcd.setCursor(0, 0);
-  	lcd.print("Press.:");
-  	lcd.setCursor(7, 0);
-  	lcd.print(pressao);
-  	lcd.setCursor(13,0);
-  	lcd.print("Bar");
-        digitalWrite(INJ1, 1);
-        digitalWrite(INJ2, 1);
-        digitalWrite(INJ3, 1);
-        digitalWrite(INJ4, 1);
-        delay(tempoentrepulsos);
-        lcd.setCursor(10, 1);
-        int strr1 = (tempototalestatico-(millis() - tempo2))/1000;
-        String strr2 = " ";
-        String str1 =  strr1 + strr2;
-        lcd.print(str1);
-        if (digitalRead(OP1) && flag1 == 1)
-        {
-          break;
-        }
-        digitalWrite(INJ1, 0);
-        digitalWrite(INJ2, 0);
-        digitalWrite(INJ3, 0);
-        digitalWrite(INJ4, 0);
-      }
-	  flag1=0;
-
   }
   
   if (flag1 == 0 && flag2 == 0)
@@ -226,10 +223,10 @@ void loop()
     digitalWrite(INJ2, 0);
     digitalWrite(INJ3, 0);
     digitalWrite(INJ4, 0);
-    digitalWrite(DRENO1,0);
-    digitalWrite(DRENO2,0);
-    digitalWrite(DRENO3,0);
-    digitalWrite(DRENO4,0);
+    digitalWrite(DRENO1,1);
+    digitalWrite(DRENO2,1);
+    digitalWrite(DRENO3,1);
+    digitalWrite(DRENO4,1);
     lcd.setCursor(0, 1);
     lcd.print("Press.:");
     lcd.setCursor(7, 1);
